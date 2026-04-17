37ade5eded507ed0c5f405f301bed161ca42421d
pennapedro3-beep <pennapedro3@gmail.com>
2026-04-17 10:35:58 -0300

    Update and rename CodScan.java to ScanearNome.java

--- ScanearNome.java
+++ ScanearNome.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class CodScan
+public class ScanearNome
 {
 	public static void main(String[] args) {
 	    Scanner sc = new Scanner(System.in); // cria objeto Scanner
