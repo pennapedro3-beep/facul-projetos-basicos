ebe89347373fbbe5c5662be66a8bfa9396375709
pennapedro3-beep <pennapedro3@gmail.com>
2026-04-17 10:56:24 -0300

    Update and rename Ex5.java to ContagemUmADez.java

--- ContagemUmADez.java
+++ ContagemUmADez.java
@@ -1,5 +1,5 @@
-
-public class Ex5
+// Enquano o numero printado for menor ou igual a 10, continuará printando por conta do sistema while
+public class ContagemUmADez
 {
 	public static void main(String[] args) {
 	    int numero110 = 1;
